50b24bb27cc0b6e8d43808d5e5ff85f0ee3abf24
Umreen <uidigitalcrafts@gmail.com>
2020-04-06 18:21:46 -0500

    adding login and register functionality

--- leadmanager/frontend/src/components/accounts/Login.js
+++ leadmanager/frontend/src/components/accounts/Login.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { login } from '../../actions/auth';
 
 export class Login extends Component {
     state = {
@@ -59,4 +62,8 @@ export class Login extends Component {
     }
 }
 
-export default Login;
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps)(Login);
